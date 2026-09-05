b8d6003f02fb0b3844b7c93d5aebc1db0ddd2d0a
Lucky Umoka <137400404+Lucky-Umoka@users.noreply.github.com>
2026-09-05 16:21:46 +0000

    worked on the phone regex compile code

--- src/main.py
+++ src/main.py
@@ -22,8 +22,8 @@ CREDIT_CARD_REGEX = re.compile(
 )
 
 
-PHONE_REGEX = re.compile(
-    r'\b(?:\+?\d{1,3}[-.\s]?)?(?:\(?\d{1,4}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,9}\b'       
+PHONE_REGEX = re.compile( 
+    r'(?<!\w)\+?\d{0,3}[-.\s]?(?:\(\d{2,4}\)[-.\s]?)?(?:\d{2,4}[-.\s]?){1,4}\d{2,4}(?!\w))'     
 )
 
 
